7cb082e490ed28b39368e7d7c7d8debb41746bab
pj090 <71534926+pj090@users.noreply.github.com>
2020-09-22 21:46:28 +0700

    Add files via upload

---
+++ cobaupload1.php
@@ -0,0 +1,10 @@
+   <!doctype html> 
+     <html> 
+     <head> 
+     <meta charset="utf-8"> 
+     <title>ujicoba upload</title> 
+     </head> 
+     <body> 
+     <?php echo "halo gais dapit disini"; ?> 
+     </body> 
+     </html>
